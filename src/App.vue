4c38f548f546c0db884f1a3501605e5332eb3ff3
David <david.perov60@gmail.com>
2021-08-24 19:51:57 +0300

    first commit!

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <Authorization msg="Registration_page"/>
+</template>
+
+<script>
+import Authorization from './components/Authorization.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Authorization
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Authorization.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>{{ text }}</div>
+</template>
+
+
+<script>
+export default {
+    name: 'Authorization',
+    props: {
+        testMessage: String
+    },
+    data() {
+        return {
+            text: 666
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
