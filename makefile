45412a029ac8f34b2fbb9336229ae64332ba76f5
david <david.perov60@gmail.com>
2021-10-25 14:39:40 +0300

    изменил параметры для отображения

--- makefile
+++ makefile
@@ -4,3 +4,5 @@ run:
 	docker run -p 80:8080 -v "$(CURDIR):/usr/src/app" -d --rm --name npo-client npo-client
 stop: 
 	docker stop npo-client
+rmi:
+	docker rmi npo-client
--- vue.config.js
+++ vue.config.js
@@ -1,27 +1,6 @@
 module.exports = {
 	devServer: {
     compress: true,
-		disableHostCheck: true, 
+		public: 'npo-automotive.ru'
   }
-  // chainWebpack: config => {
-  //   config.module
-  //     .rule('vue')
-	// 		.user('vue-style-loader')
-  //       .tap(options => {
-  //         // modify the options...
-  //         return options
-  //       })
-  // }
-	// module: {
-	// 	rules: [
-	// 		{
-	// 			test: /\.scss$/,
-	// 			use: [
-	// 				'vue-style-loader',
-  //         'css-loader',
-  //         'sass-loader'
-	// 			]
-	// 		}
-	// 	]
-	// }
 }
