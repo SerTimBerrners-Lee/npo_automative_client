c0071bcca6f1f3f750115b5b8b249818815c3486
david <david.perov60@gmail.com>
2021-10-25 16:04:19 +0300

    Добавил переменные окружения для продакшена cross-env

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 module.exports = {
-	devServer: {
+	devServer: process.env.VUE_APP_IS_PROD === 'true' ?	{
     compress: true,
+		public: 'npo-automotive.ru'
+  } : {
+		compress: true,
 		disableHostCheck: true
-  }
+	}
 }
