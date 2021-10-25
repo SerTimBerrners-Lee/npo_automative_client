49c1e5137fcda2794010d573ba9d4227184137ea
david <david.perov60@gmail.com>
2021-10-25 16:16:51 +0300

    Удалил консольки

--- src/js/path.js
+++ src/js/path.js
@@ -1,5 +1,3 @@
 const PATH_TO_SERVER = `http://${process.env.VUE_APP_PATH_TO_SERVER}:5000/`
-console.log("PATH:", process.env.VUE_APP_PATH_TO_SERVER)
-console.log("IS_PROD:", process.env.VUE_APP_IS_PROD)
 
 export default PATH_TO_SERVER;
