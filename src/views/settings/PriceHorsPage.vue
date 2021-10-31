01f99503288cbb693be77bca5e5ce54d4e0f6bfd
david <david.perov60@gmail.com>
2021-10-31 23:24:39 +0300

    Норма-час

--- src/views/settings/PriceHorsPage.vue
+++ src/views/settings/PriceHorsPage.vue
@@ -8,27 +8,75 @@
           <th>Ед.</th>
           <th>Значение</th>
         </tr>
-        <tr class="td-row">
-          <td>Стоимость норма-часа</td>
-          <td>руб.</td>
-          <td>199</td>
+        <tr class="td-row" v-if='norm_hors'>
+          <td>{{ norm_hors.name }}</td>
+          <td>{{ norm_hors.ez }}</td>
+          <td>
+            <input style='width: 100px;' type="number" min='1' v-model='norm_hors.znach'>
+          </td>
         </tr>
       </table>
-      <div class="btn-control"  v-if="getRoleAssets && getRoleAssets.assets.settingsAssets.edit">
-        <button class="btn-small btn-add">Сохранить</button>
+      <div class="btn-control" style='margin-top: 10px;'  v-if="getRoleAssets && getRoleAssets.assets.settingsAssets.edit">
+        <button class="btn-small btn-add" @click='update'>Сохранить</button>
       </div>
     </div>
+    <InformFolder  
+      :title='titleMessage'
+      :message = 'message'
+      :type = 'type'
+      v-if='showInformPanel'
+      :key='keyInformTip'
+    />
   </div>
 </template>
 
 <script>
-import {mapGetters} from 'vuex'
+import {mapGetters} from 'vuex';
+import PATH_TO_SERVER from '@/js/path';
+import { showMessage } from '@/js/';
+import InformFolder from '@/components/InformFolder.vue';
 export default {
   data() {
     return{
+      norm_hors: null,
 
+      titleMessage: '',
+      message: '',
+      type: '',
+      showInformPanel: false,
+      keyInformTip: 0,
     }
   },
-  computed: mapGetters(['getRoleAssets'])
+  computed: mapGetters(['getRoleAssets']),
+  components: {InformFolder},
+  methods: {
+    async update() {
+      const res = await fetch(`${PATH_TO_SERVER}api/settings/normhors`, {
+        method: 'post',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          znach: this.norm_hors.znach
+        })
+      })
+      if(res.ok)
+        showMessage('', 'Значение успешно обновлено', 's', this)
+        else 
+          showMessage('', 'Произошла ошибка при обновлении значения', 'e', this)
+    }
+  },
+  async mounted() {
+    const res = await fetch(`${PATH_TO_SERVER}api/settings/normhors`)
+    if(res.ok) {
+      try {
+        const result = await res.json()
+        this.norm_hors = result[0]
+      } catch (e) {
+        console.log(e)
+      }
+    }
+  }
 }
 </script>
