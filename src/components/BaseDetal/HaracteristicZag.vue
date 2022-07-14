17ce1c24bdd51e19e0983aba19b9769a887badfc
david <david.perov60@gmail.com>
2022-07-14 10:21:26 +0300

    Удалил добавление заготовок

--- src/components/BaseDetal/HaracteristicZag.vue
+++ src/components/BaseDetal/HaracteristicZag.vue
@@ -221,7 +221,7 @@ export default {
 			density: 0,
 		}
 	},
-	components: { ModalBaseMaterial, AddHar },
+	components: { ModalBaseMaterial },
 	methods: {
 		...mapActions(['getOneTypeMaterial']),
 		// ТОЛЬКО ПЕРЕМЕННЫЕ ЗНАЧЕНИЯ МОЖНО РЕДАКТИРОВАТЬ !!!
